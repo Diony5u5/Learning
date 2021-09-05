52e0c8944fe6e5306262b1034ec97da981c5c5db
andornot <wanghaoyang@live.com>
2021-09-05 15:45:20 +0800

    eslint

--- 06.Node.js/hellonodejs/index.js
+++ 06.Node.js/hellonodejs/index.js
@@ -5,13 +5,13 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content_Type','text/plain');
+    res.setHeader('Content_Type', 'text/plain');
     res.write('Hello NodeJS');
     res.end();
 });
 
-server.listen(port,hostname,()=>{
+server.listen(port, hostname, () => {
     console.log('服务器已经启用');
 });
--- 06.Node.js/lanserver/index.js
+++ 06.Node.js/lanserver/index.js
@@ -1,65 +1,65 @@
-var http = require('http');
-var url = require('url');
-var path = require('path');
-var fs = require('fs');
-var mime = require('./mime');
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+var mime = require("./mime");
  
 //服务器端口号
 var port = 8888;
 //服务器路径
-var root = 'E:/Learning/02.CSS/00_exercise';
+var root = "E:/Learning/02.CSS/00_exercise";
 //默认访问根目录下的"index.html"
-var index = '14_立方体.html';
+var index = "14_立方体.html";
  
 http.createServer(function (request, response) {
  
-    var realPath = url.parse(request.url).pathname;
+	var realPath = url.parse(request.url).pathname;
  
-    //默认访问根目录下的index.html
-    if (realPath.charAt(realPath.length - 1) == "/"){
-        realPath += index;
-    }
+	//默认访问根目录下的index.html
+	if (realPath.charAt(realPath.length - 1) == "/"){
+		realPath += index;
+	}
  
-    //安全问题，禁止父路径
-    realPath = realPath.replace(/\.\./g, '');
-    var realPath = root + realPath;
+	//安全问题，禁止父路径
+	realPath = realPath.replace(/\.\./g, "");
+	realPath = root + realPath;
  
-    //获取文件的后缀名，为待会的MIME类型提供支持
-    var ext = path.extname(realPath);
-    ext = ext ? ext.slice(1) : 'unknown';
+	//获取文件的后缀名，为待会的MIME类型提供支持
+	var ext = path.extname(realPath);
+	ext = ext ? ext.slice(1) : "unknown";
  
-    //先判断访问文件是否存在，并返回对应的HTTP状态码，再读取静态文件
-    fs.exists(realPath, function (exists) {
-        console.log('path.exists--%s', exists);
-        if (!exists) {
-            response.writeHead(404, {
-                'Content-Type': 'text/plain'
-            });
-            response.write("This request URL " + realPath + " was not found on this server.");
-            response.end();
-        } else {
-            fs.readFile(realPath, "binary", function (err, file) {
-                if (err) {
-                    response.writeHead(500, {
-                        'Content-Type': 'text/plain'
-                    });
+	//先判断访问文件是否存在，并返回对应的HTTP状态码，再读取静态文件
+	fs.exists(realPath, function (exists) {
+		console.log("path.exists--%s", exists);
+		if (!exists) {
+			response.writeHead(404, {
+				"Content-Type": "text/plain"
+			});
+			response.write("This request URL " + realPath + " was not found on this server.");
+			response.end();
+		} else {
+			fs.readFile(realPath, "binary", function (err, file) {
+				if (err) {
+					response.writeHead(500, {
+						"Content-Type": "text/plain"
+					});
  
-                    response.end(err + '');
-                } else {
-                    //MIME类型支持
-                    var contentType = mime[ext] || "text/plain";
-                    response.writeHead(200, {
-                        'Content-Type': contentType
-                    });
+					response.end(err + "");
+				} else {
+					//MIME类型支持
+					var contentType = mime[ext] || "text/plain";
+					response.writeHead(200, {
+						"Content-Type": contentType
+					});
  
-                    response.write(file, "binary");
-                    response.end();
-                }
-            });
-        }
-    });
+					response.write(file, "binary");
+					response.end();
+				}
+			});
+		}
+	});
  
 }).listen(port);
  
 // 终端打印运行信息
-console.log('服务器运行在端口:' + port);
+console.log("服务器运行在端口:" + port);
